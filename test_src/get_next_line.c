d9094dbaf108a283043ede2cfb1681a382879c11
m3g4d1v3r <marlon365x@protonmail.ch>
2022-06-27 20:51:23 -0300

    Add test file

--- test_src/get_next_line.c
+++ test_src/get_next_line.c
@@ -5,6 +5,7 @@
 #include "../get_next_line.h"
 
 Test(gnl, simple0) {
+	#define BUFFER_SIZE 1
 	FILE *ptrSimple;
 	int fd;
 	ptrSimple = fopen("simple0.txt", "w+");
@@ -27,6 +28,26 @@ Test(gnl, simple0) {
 	cr_assert(eq(str, res, "A"));
 	free(res);
 	res = get_next_line(fd);
-	cr_assert(ne(str, res, "B"));
+	cr_assert(res == NULLPTR);
+	free(res);
+}
+
+Test(gnl, simple1) {
+	#define BUFFER_SIZE 1
+	FILE *ptrSimple;
+	int fd;
+	ptrSimple = fopen("simple1.txt", "w+");
+	fd = fileno(ptrSimple);
+	fputs("01234567890123456789012345678901234567890", ptrSimple);
+	fclose(ptrSimple);
+	fd = open("simple1.txt", O_RDWR);
+	printf("file descriptor: %d\n", fd);
+	
+	char *res;
+	res = get_next_line(fd);
+	cr_assert(eq(str, res, "01234567890123456789012345678901234567890"));
+	free(res);
+	res = get_next_line(fd);
+	cr_assert(res == NULLPTR);
 	free(res);
 }
